3211f9670019ebf6a12f2d17638ba2ecb33c757d
unknown <hydham007@gmail.com>
2022-10-03 17:47:05 +0530

    upadating a car

--- main.py
+++ main.py
@@ -1,59 +1,48 @@
-from operator import mod
-from optparse import Option
-from urllib import response
-from xmlrpc.client import Boolean
 from fastapi import FastAPI, Query, Path, HTTPException, status, Body
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, Field
-from typing import Dict, List, Optional
-from database import cars
+from typing import Optional, List, Dict
 
+from starlette.status import HTTP_400_BAD_REQUEST
+from database import cars
 class Car(BaseModel):
     make: Optional[str]
     model: Optional[str]
-    year: Optional[int] = Field(..., ge= 1997, lt= 2022)
+    year: Optional[int] = Field(...,ge=1970,lt=2022)
     price: Optional[float]
-    engine: Optional[str] = "v4"
+    engine: Optional[str] = "V4"
     autonomous: Optional[bool]
     sold: Optional[List[str]]
-
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"Hello to my first fastAPI ": "World"}
+    return {"Welcome to": "your first API in FastAPI!"}
 
-@app.get("/cars", response_model= List[Dict[str, Car]])
-def get_cars(number: Optional[str] = Query(10, max_length=3)):
+@app.get("/cars", response_model=List[Dict[str, Car]])
+def get_cars(number: Optional[str] = Query("10",max_length=3)):
     response = []
-
-    for id, car in list(cars.items()) [:int (number)]:
+    for id, car in list(cars.items())[:int(number)]:
         to_add = {}
         to_add[id] = car
         response.append(to_add)
-
     return response
 
-@app.get("/cars/{id}", response_model= Car)
-def get_cars_by_id(id: int = Path(..., gt=0, lt = 1000)):
-
+@app.get("/cars/{id}", response_model=Car)
+def get_car_by_id(id: int = Path(...,ge=0,lt=1000)):
     car = cars.get(id)
-
     if not car:
-        raise(HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "no id to search"))
-
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Could not find car by ID.")
     return car
 
 @app.post("/cars", status_code=status.HTTP_201_CREATED)
 def add_cars(body_cars: List[Car], min_id: Optional[int] = Body(0)):
     if len(body_cars) < 1:
-        raise(HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "No cars added"))
-
-    min_id = len(cars.values()) + min_id #fixing the id for the car -> length of the cars dict + offset from the request body
-
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST,detail="No cars to add.")
+    min_id = len(cars.values()) + min_id
     for car in body_cars:
-        while cars.get(min_id): #checking the id selected is not returning car, if yes increment id by 1 and check until false
+        while cars.get(min_id):
             min_id += 1
         cars[min_id] = car
         min_id += 1
